512f6b1fa5d619d8aa0b6b366b7c0ead424e329b
Gooseman <fdbl;gmnh>
2021-04-29 20:43:19 +0300

    Buttons now do stuff

--- app/src/main/java/com/test/counter/MainActivity.java
+++ app/src/main/java/com/test/counter/MainActivity.java
@@ -3,6 +3,7 @@ package com.test.counter;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,22 +17,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView counterValueTV = findViewById(R.id.counter_value);
+        counterValueTV.setText(String.valueOf(mCounterValue));
+
+        findViewById(R.id.plus_button).setOnClickListener(v -> {
+            mCounterValue++;
+            counterValueTV.setText(String.valueOf(mCounterValue));
+        });
+
+        findViewById(R.id.minus_button).setOnClickListener(v -> {
+            mCounterValue--;
+            counterValueTV.setText(String.valueOf(mCounterValue));
+        });
+
+        findViewById(R.id.reset_button).setOnClickListener(v -> {
+            mCounterValue = 0;
+            counterValueTV.setText(String.valueOf(mCounterValue));
+        });
 
     }
-
-    public void plusClicked(View view) {
-        Log.d(LOG_TAG, "Plus clicked");
-    }
-
-    public void minusClicked(View view) {
-        Log.d(LOG_TAG, "minus clicked");
-    }
-
-    public void resetClicked(View view) {
-        Log.d(LOG_TAG, "reset clicked");
-    }
+//    public void plusClicked(View view) {
+//        Log.d(LOG_TAG, "Plus clicked");
+//        mCounterValue++;
+//    }
+//
+//    public void minusClicked(View view) {
+//        Log.d(LOG_TAG, "minus clicked");
+//        mCounterValue--;
+//    }
+//
+//    public void resetClicked(View view) {
+//        Log.d(LOG_TAG, "reset clicked");
+//        mCounterValue = 0;
+//    }
 
     public void showToastMessage(CharSequence message) {
         Toast.makeText(this.getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
+
+
 }
