0c97c3ab1bbb22f4368a05c72724de8aabf9a92e
Gooseman <fdbl;gmnh>
2021-04-29 22:11:03 +0300

    Saves data to shared preferences

--- app/src/main/java/com/test/counter/MainActivity.java
+++ app/src/main/java/com/test/counter/MainActivity.java
@@ -1,55 +1,59 @@
 package com.test.counter;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.preference.PreferenceManager;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final String PREF_KEY = "COUNTER_VALUE";
     private static final String LOG_TAG = MainActivity.class.getName();
-    private int mCounterValue;
+    private SharedPreferences mData;
+    private TextView mCounterValueTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView counterValueTV = findViewById(R.id.counter_value);
-        counterValueTV.setText(String.valueOf(mCounterValue));
-
+        mData = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+        mCounterValueTv = findViewById(R.id.counter_value);
+        updateValue(getValue());
         findViewById(R.id.plus_button).setOnClickListener(v -> {
-            mCounterValue++;
-            counterValueTV.setText(String.valueOf(mCounterValue));
+            updateValue(getValue() + 1);
         });
 
         findViewById(R.id.minus_button).setOnClickListener(v -> {
-            mCounterValue--;
-            counterValueTV.setText(String.valueOf(mCounterValue));
+            updateValue(getValue() - 1);
         });
 
         findViewById(R.id.reset_button).setOnClickListener(v -> {
-            mCounterValue = 0;
-            counterValueTV.setText(String.valueOf(mCounterValue));
+            int oldCounterValue = getValue();
+            updateValue(0);
+            Snackbar.make(v, "Counter reset", BaseTransientBottomBar.LENGTH_SHORT)
+                    .setAction("Undo", ignored -> {
+                        updateValue(oldCounterValue);
+                        showToastMessage("Undone");
+                    })
+                    .show();
         });
 
     }
-//    public void plusClicked(View view) {
-//        Log.d(LOG_TAG, "Plus clicked");
-//        mCounterValue++;
-//    }
-//
-//    public void minusClicked(View view) {
-//        Log.d(LOG_TAG, "minus clicked");
-//        mCounterValue--;
-//    }
-//
-//    public void resetClicked(View view) {
-//        Log.d(LOG_TAG, "reset clicked");
-//        mCounterValue = 0;
-//    }
+
+    private void updateValue(int value) {
+        mData.edit().putInt(PREF_KEY, value).apply();
+        mCounterValueTv.setText(String.valueOf(value));
+    }
+
+    private int getValue() {
+        return mData.getInt(PREF_KEY, 0);
+    }
 
     public void showToastMessage(CharSequence message) {
         Toast.makeText(this.getApplicationContext(), message, Toast.LENGTH_SHORT).show();
